2804dd3263cf19358ef2ef38443c10aa7bac6bee
david <davidaccomazzo@gmail.com>
2019-05-26 10:45:42 -0700

    cleanup of unpushed files

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <title>Todo List App</title>
-<link rel="stylesheet" href="includes/css/style.css">
+<link rel="stylesheet" href="./sass/style.scss">
 </head>
 
 <body>
@@ -48,7 +48,28 @@
 
 </form>
 
-
+<h3>Submit a task for a user</h3>
+<form action="actions/addatask.php" method="post">
+    <table>
+        <tbody>
+            <tr>
+                <td><label>Username</td>
+                <td><input type="text" name="username"></label></td>
+            </tr>
+            <tr>
+                <td><label>Task Name</td>
+                <td><input type="text" name="taskname"></label></td>
+            </tr>
+            <tr>
+                <td><label>Task Description</td>
+                <td><input type="text" name="taskdescription"></label></td>
+            </tr>
+            <tr>
+                <td><input type="submit" value="Add Task"></td>
+            </tr>
+        </tbody>
+    </table>
+</form>
 
 </body>
 </html> 
