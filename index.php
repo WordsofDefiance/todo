73a0e8321045450708faf08fadde6e69ce8c2e85
david <davidaccomazzo@gmail.com>
2019-05-30 20:59:10 -0700

    removing unnecessary form and action

--- index.php
+++ index.php
@@ -9,29 +9,6 @@
 <h1>Todo List</h1>
 
 
-<form action="actions/formhandler.php" method="post">
-<table>
-<tbody>
-<tr>
-<td>
-<label>Name</td>
-<td><input type="text" name="name"></label></td>
-</tr>
-<tr>
-<td><label>Task</td>
-<td><input type="text" name="task"></label></td>
-</tr>
-<tr>
-<td><label>Task Description</td>
-<td><input type="text" name="description"></label></td>
-</tr>
-<tr><td>
-<input type="submit" value="Add Task">
-</td></tr>
-</form>
-</tbody>
-</table>
-
 <h3>Submit a user name to see their tasks</h3>
 <form action="actions/fetchusertasks.php" method="get">
 <table> 
