1234158ddbb72b232783ec80f2685de513f40dec
david <davidaccomazzo@gmail.com>
2019-06-02 13:45:24 -0700

    styling the buttons

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
  <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <title>Todo List App</title>
 <link rel="stylesheet" href="./css/style.min.css">
@@ -7,62 +7,34 @@
 </head>
 
 <body>
-<h1>Todo List</h1>
+	<main id="main">
+		<h1 class="app-title focus-in-contract" >Todo List</h1>
 
 
-<h3>Submit a user name to see their tasks</h3>
-<form action="actions/fetchusertasks.php" method="get">
-<table> 
-<tbody>
-<tr>
-<td><label>Username</td>
-<td><input type="text" name="username"></label></td>
-</tr>
-<tr><td>
-<input class="shadow-drop-center" type="submit" value="Fetch Tasks">
-</td></tr>
-</tbody>
-</table>
+		<h3>Submit a user name to see their tasks</h3>
+		<form name="Fetch User Tasks" action="actions/fetchusertasks.php" method="get">
+			<label for="fetch-username">Username
+			<input id="fetch-username" type="text" name="username"></label>
+			<input class="shadow-pop" type="submit" value="Fetch Tasks">
+		</form>
 
-</form>
+		<h3>Submit a task for a user</h3>
+		<form name="Add a task" action="actions/addatask.php" method="post">
+			<label>Username
+			<input type="text" name="username"></label>
+			<label>Task Name
+			<input type="text" name="taskname"></label>
+			<label>Task Description
+			<input type="text" name="taskdescription"></label>
+			<input class="shadow-pop" type="submit" value="Add Task">
+		</form>
 
-<h3>Submit a task for a user</h3>
-<form action="actions/addatask.php" method="post">
-    <table>
-        <tbody>
-            <tr>
-                <td><label>Username</td>
-                <td><input type="text" name="username"></label></td>
-            </tr>
-            <tr>
-                <td><label>Task Name</td>
-                <td><input type="text" name="taskname"></label></td>
-            </tr>
-            <tr>
-                <td><label>Task Description</td>
-                <td><input type="text" name="taskdescription"></label></td>
-            </tr>
-            <tr>
-                <td><input class="shadow-drop-center" type="submit" value="Add Task"></td>
-            </tr>
-        </tbody>
-    </table>
-</form>
-
-<h3>Add A User</h3>
-<form action="actions/addauser.php" method="post">
-    <table>
-        <tbody>
-            <tr>
-                <td><label>Username</td>
-                <td><input type="text" name="username"></label></td>
-            </tr>
-            <tr>
-                <td><input type="submit" value="Add User"></td>
-            </tr>
-        </tbody>
-    </table>
-</form>
-
+		<h3>Add A User</h3>
+		<form name="Add a user" action="actions/addauser.php" method="post">
+			<label>Username
+			<input type="text" name="username"></label>
+			<input class="shadow-pop" type="submit" value="Add User">
+		</form>
+		</main>
 </body>
 </html> 
